74026a11a3d51e40513b48234d101cbd5c3d8062
Mohamed-Mabchour <149189198+MABDex@users.noreply.github.com>
2025-11-30 17:19:16 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,11 +7,11 @@ PORT=${PORT:-8080}
 
 echo "Starte McpSpring-Server auf Port 8899 ..."
 java -jar McpSpring-Server-0.0.1-SNAPSHOT.jar --server.port=8899 &
-sleep 20
+sleep 180
 
 echo "Starte McpSpring-client auf Port 8066 ..."
 java -jar McpSpring-client-0.0.1-SNAPSHOT.jar --server.port=8066 &
-sleep 20
+sleep 180
 
 echo "Starte ChatbotAlge auf Port 8070 ..."
 java -jar ChatbotAlge-0.0.1-SNAPSHOT.jar --server.port=8070 &
