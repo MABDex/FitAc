14a17e695eb51588dd07a9fbb9ec4842ce6dc140
Mohamed-Mabchour <149189198+MABDex@users.noreply.github.com>
2025-11-30 15:52:07 +0100

    Update AiRestController.java

--- McpSpring-client/src/main/java/org/example/mcpspringclient/RestController/AiRestController.java
+++ McpSpring-client/src/main/java/org/example/mcpspringclient/RestController/AiRestController.java
@@ -2,6 +2,8 @@ package org.example.mcpspringclient.RestController;
 
 
 import org.example.mcpspringclient.Agents.AiAgent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
