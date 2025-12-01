5f734a01c74773e13fd93cbcba146c8b074c511c
Mohamed-Mabchour <149189198+MABDex@users.noreply.github.com>
2025-12-01 15:31:03 +0100

    Update AzureOpenAIConfig.java

--- McpSpring-client/src/main/java/org/example/mcpspringclient/AzureOpenAIConfig.java
+++ McpSpring-client/src/main/java/org/example/mcpspringclient/AzureOpenAIConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AzureOpenAIConfig {
+public class AzureOpenAIConfig{
 
     @Bean
     public HttpClient azureHttpClient() {
