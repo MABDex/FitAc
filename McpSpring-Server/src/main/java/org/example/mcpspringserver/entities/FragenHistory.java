53a1edb5de4d15518179d0cb9c6ce87a61a2bda2
Mohamed-Mabchour <149189198+MABDex@users.noreply.github.com>
2026-02-01 20:52:30 +0100

    Update FragenHistory.java

--- McpSpring-Server/src/main/java/org/example/mcpspringserver/entities/FragenHistory.java
+++ McpSpring-Server/src/main/java/org/example/mcpspringserver/entities/FragenHistory.java
@@ -19,7 +19,7 @@ public class FragenHistory {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(columnDefinition = "TEXT")
