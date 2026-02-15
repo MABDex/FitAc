b6e09fd3bf1be7e1cf041f36423b6aced9ebd655
Mohamed-Mabchour <149189198+MABDex@users.noreply.github.com>
2026-02-15 15:10:44 +0100

    Create ChatConfig.java

---
+++ McpSpring-client/src/main/java/org/example/mcpspringclient/Agents/ChatConfig.java
@@ -0,0 +1,14 @@
+package org.example.mcpspringclient.Agents;
+
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatConfig {
+    @Bean
+    public ChatMemory chatMemory() {
+        return MessageWindowChatMemory.builder().maxMessages(10).build();
+    }
+}
