7f46c2764195552448e6b0d763b3a70dbf8ebd4e
Mohamed-Mabchour <149189198+MABDex@users.noreply.github.com>
2026-02-15 15:56:51 +0100

    Update AiAgent.java

--- McpSpring-client/src/main/java/org/example/mcpspringclient/Agents/AiAgent.java
+++ McpSpring-client/src/main/java/org/example/mcpspringclient/Agents/AiAgent.java
@@ -17,50 +17,61 @@ import org.springframework.stereotype.Service;
 @Service
 public class AiAgent {
 
+private final ChatClient.Builder chatClientBuilder;
+    private final ToolCallbackProvider toolCallbackProvider;
+    private final ChatMemory chatMemory;
 
-    private ChatClient chatClient;
-                                                  // Object contient la Liste des Tools
-    public AiAgent(ChatClient.Builder chatClient , ToolCallbackProvider toolCallbackProvider , ChatMemory chatMemory) {
-        this.chatClient = chatClient
-                .defaultToolCallbacks(toolCallbackProvider)
-                .defaultAdvisors(
-                        MessageChatMemoryAdvisor.builder(chatMemory).build()
-                )
-                .defaultSystem( """
-                        You are an AI Assistant that answers user questions about recipes
-                        
-                         -Translate the user's question into English if necessary,
-                         -The SPARQL query has already been generated and executed by the MCP-server,
-                         -your task is to read the JSON results returned from the query,
-                         -Explain the answer to the user in natural language (same language as the user),Always include the following properties: 
-                           1) recipe name,
-                           2) description, 
-                           3) ingredients, 
-                           4) recipeIngredients , 
-                           5) recipeInstructions (strictly include the instructions as text, do NOT provide any links)
-                         
-                         
-                         -If the user asks about prices or costs of ingredients,you must include the price for every ingredient in each recipe. always use Only the provided database tools getAllIngredients() to fetch the actual prices from the database and include them in the answer.
-                         -If the user does not ask about prices, strictly do not include any price information.
-                         -If no results are found, politely inform the user do NOT generate SPARQL queries
-                         
-                         Important rules:
-                        - Always use the provided tools, do NOT invent answers without querying the data.
-                        - If no matching recipes are found, politely inform the user.
-                        - Include both the SPARQL query and the natural answer if the user might need it.
-                        - Always include the estimated price of the recipe and the price of each ingredient in your response.
-                       
-                                    """ )
-                .build();
+    public AiAgent(ChatClient.Builder chatClientBuilder,
+                   ToolCallbackProvider toolCallbackProvider,
+                   ChatMemory chatMemory) {
+
+        this.chatClientBuilder = chatClientBuilder;
+        this.toolCallbackProvider = toolCallbackProvider;
+        this.chatMemory = chatMemory;
     }
 
-    public String askLLM(String conversationId , String query){
+    public String askLLM(String conversationId, String query) {
+
+        ChatClient chatClient = chatClientBuilder
+                .defaultToolCallbacks(toolCallbackProvider)
+                .defaultAdvisors(
+                        MessageChatMemoryAdvisor.builder(chatMemory)
+                                .conversationId(conversationId)
+                                .build()
+                )
+                .defaultSystem("""
+                        You are an AI Assistant that answers user questions about recipes
+                        
+                                                 -Translate the user's question into English if necessary,
+                                                 -The SPARQL query has already been generated and executed by the MCP-server,
+                                                 -your task is to read the JSON results returned from the query,
+                                                 -Explain the answer to the user in natural language (same language as the user),Always include the following properties:
+                                                   1) recipe name,
+                                                   2) description,
+                                                   3) ingredients,
+                                                   4) recipeIngredients ,
+                                                   5) recipeInstructions (strictly include the instructions as text, do NOT provide any links)
+                        
+                        
+                                                 -If the user asks about prices or costs of ingredients,you must include the price for every ingredient in each recipe. always use Only the provided database tools getAllIngredients() to fetch the actual prices from the database and include them in the answer.
+                                                 -If the user does not ask about prices, strictly do not include any price information.
+                                                 -If no results are found, politely inform the user do NOT generate SPARQL queries
+                        
+                                                 Important rules:
+                                                - Always use the provided tools, do NOT invent answers without querying the data.
+                                                - If no matching recipes are found, politely inform the user.
+                                                - Include both the SPARQL query and the natural answer if the user might need it.
+                                                - Always include the estimated price of the recipe and the price of each ingredient in your response.
+                        
+                        """)
+                .build();
+
         return chatClient.prompt()
-                .conversationId(conversationId)
                 .user(query)
                 .call()
                 .content();
     }
+
 
 
 
